497e976a2b0044ec5c69db3e04b7f482d15a1481
vteso <vtesoho@163.com>
2019-12-02 20:50:31 +0800

    other_shutdown

--- other_shutdown/main.go
+++ other_shutdown/main.go
@@ -33,9 +33,11 @@ func main() {
 
 	ctx,cancel:=context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	if err:=srv.Shutdown(ctx);err!=nil{
 		log.Fatal("sever shutdown:",err)
 	}
+
 	log.Println("sever exiting")
 
 }
